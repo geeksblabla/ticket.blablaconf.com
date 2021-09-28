dddcb3c957a6bd5148bd2aa2b84f9dbc500fcb69
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-09-28 15:33:04 +0100

    fix seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config.js";
+import Head from "next/head";
+import Router from "next/router";
+import * as gtag from "../components/gtag";
+
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+
+export default MyApp;
